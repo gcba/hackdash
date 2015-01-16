e246c692629475767b8e469fb08f9b51677a2ad4
Luis Agüero <agueroluisalberto@gmail.com>
2015-01-16 17:49:46 -0300

    coming soon projects

--- public/ng-client/modules/home/homeCtrl.js
+++ public/ng-client/modules/home/homeCtrl.js
@@ -1,12 +1,22 @@
-ocApp.controller('homeCtrl', function($scope, Restangular, $location, $anchorScroll) {
+ocApp.controller('homeCtrl', function($scope, Restangular, $location, $anchorScroll, $rootScope, $routeParams) {
 
 	$scope.challenges = Restangular.all('dashboards').getList().$object;
 
 	$scope.projects = Restangular.all('projects').getList().$object;
 	
+	$scope.userCanEdit = true;
+
 	$scope.goToChallenges = function (){
 	    $location.hash('challenge-container');
 	    $anchorScroll();
-	};
+	}
 
+	$scope.canEdit = function(challengeId){
+		if(!challengeId){
+			var challengeId = "2";
+		}
+		$scope.userCanEdit = ($rootScope.user.admin_in.indexOf(challengeId) >= 0);
+		return $scope.userCanEdit;
+
+	};
 });
